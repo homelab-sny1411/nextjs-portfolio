428fb45beb5983cf9281a76391ff307133511e0f
sny1411 <matteo2004@outlook.fr>
2025-06-30 21:30:58 +0200

    :bug: correction date footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,7 +1,11 @@
 const Footer = () => {
     return (
         <footer className="w-full bg-gray-900 text-white text-center py-4 mt-auto">
-            © 2024 - Portfolio Humez Mattéo. Tous droits réservés.
+            <div className="container mx-auto">
+                <p className="text-sm">
+                    © {new Date().getFullYear()} Portfolio. Tous droits réservés.
+                </p>
+            </div>
         </footer>
     );
 };
