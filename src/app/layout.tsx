476267c33478f85bf2fc484f059f44f27c1d1574
sny1411 <matteo2004@outlook.fr>
2025-10-06 21:21:55 +0200

    :bug: correction background sur les téléphones

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="fr">
-        <body className="overscroll-none">
+        <body className="overscroll-none bg-gray-900">
         {children}
         </body>
         </html>
